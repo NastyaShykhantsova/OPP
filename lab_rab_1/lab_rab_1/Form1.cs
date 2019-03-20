f416e92573f67dd382e1012cae439c6083a8fb90
NastyaShykhantsova <NastyaShykhantsova@users.noreply.github.com>
2019-03-20 18:12:58 +0300

    something

--- lab_rab_1/lab_rab_1/Form1.cs
+++ lab_rab_1/lab_rab_1/Form1.cs
@@ -36,83 +36,243 @@ namespace lab_rab_1
         public Color curColor;
         public List<Point> ps;
         MyImage myIm = new MyImage();
+
         public fMain()
         {
-
+            InitializeComponent();
+            press = false;
         }
-
-
+        
 
         private void bLine_Click(object sender, EventArgs e)
         {
-
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drLine = true;
+            FigFalse();
+            drLine = true;
+            bLine.BackColor = Color.Tomato;
+            curb = bLine;
         }
 
         private void bRectangle_Click(object sender, EventArgs e)
         {
-
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drRect = true;
+            bRectangle.BackColor = Color.Tomato;
+            curb = bRectangle;
         }
 
         private void bEllipse_Click(object sender, EventArgs e)
         {
- 
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drEll = true;
+            bEllipse.BackColor = Color.Tomato;
+            curb = bEllipse;
         }
 
         private void bTriangle_Click(object sender, EventArgs e)
         {
-
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drPol = true;
+            bPolygon.BackColor = Color.Tomato;
+            curb = bPolygon;
         }
 
         private void bSquare_Click(object sender, EventArgs e)
         {
-          
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drSqu = true;
+            bSquare.BackColor = Color.Tomato;
+            curb = bSquare;
         }
 
         private void bCircle_Click(object sender, EventArgs e)
         {
-
+            PolEndDraw();
+            if (curb != null)
+            {
+                curb.BackColor = SystemColors.Control;
+            }
+            FigFalse();
+            drCir = true;
+            bCircle.BackColor = Color.Tomato;
+            curb = bCircle;
         }
 
         private void pbHolst_MouseUp(object sender, MouseEventArgs e)
         {
+            if ((fig != null) && (!drPolProc) && (!drPolEnd))
+            {
+                Pen pen = new Pen(curColor, thickness);
+                press = false;
+                Graphics g = Graphics.FromImage(myIm.bmp);
+                g.DrawImage(myIm.bmp, 0, 0);
+                g.DrawImage(myIm.bmp_2, 0, 0);
+                pb.Image = myIm.bmp;
+            }
+            if (drPolEnd)
+            {
+                drPolEnd = false;
+                Pen pen = new Pen(curColor, thickness);
+                press = false;
+                Graphics g = Graphics.FromImage(myIm.bmp);
+                fig = new Poligon(ps, pen, pos1, pos2);
+                fig.mypen = pen;
+                fig.Draw(g);
+                g.DrawImage(myIm.bmp, 0, 0);
+                myIm.figures.Add(fig);
+                pb.Image = myIm.bmp;
+                pb.Refresh();
+                g.Save();
+            }
+        }
 
+        public void FigFalse()
+        {
+            drLine = false;
+            drRect = false;
+            drCir = false;
+            drEll = false;
+            drSqu = false;
+            drPol = false;
         }
 
 
         private void cbThickness_SelectedIndexChanged(object sender, EventArgs e)
         {
-          
+            PolEndDraw();
+            thickness = Convert.ToInt32(cbThickness.Text);
         }
 
         private void pbHolst_MouseDown(object sender, MouseEventArgs e)
         {
-            
+            Pen pen = new Pen(curColor, thickness);
+            pos1 = e.Location;
+            press = true;
+            if (drCir)
+            {
+                fig = new Circle(pen, pos1, pos2);
+
+            }
+            else if (drEll)
+            {
+                fig = new Ellipse(pen, pos1, pos2);
+
+            }
+            else if (drLine)
+            {
+                fig = new Line(pen, pos1, pos2);
+            }
+            else if (drRect)
+            {
+                fig = new RectangleSpace.Rectangle(pen, pos1, pos2);
+            }
+            else if (drSqu)
+            {
+                fig = new Square(pen, pos1, pos2);
+            }
+            else if (drPolProc)
+            {
+                if (((Math.Abs(ps[0].X - pos1.X) < 5) && (Math.Abs(ps[0].Y - pos1.Y) < 5) && (ps.Count > 1)))
+                {
+                    ps.Add(ps[0]);
+                    drPolProc = false;
+                    drPolEnd = true;
+                }
+                else
+                {
+                    Graphics g = Graphics.FromImage(pb.Image);
+                    Pen p = new Pen(Color.Yellow, 1);
+                    g.DrawLine(p, ps[ps.Count - 1], pos1);
+                    pb.Refresh();
+                    g.Save();
+                    ps.Add(pos1);
+                    p.Dispose();
+                }
+            }
+            else if (drPol)
+            {
+                ps = new List<Point>();
+                ps.Add(pos1);
+                drPol = false;
+                drPolProc = true;
+            }
         }
 
         private void fMain_Load(object sender, EventArgs e)
         {
-
+            pb = pbHolst;
+            myIm.bmp = new Bitmap(550, 470);
+            myIm.bmp_2 = new Bitmap(550, 470);
+            myIm.figures = new List<Shape>();
+            OpenFromFile();
+            pb.Image = myIm.bmp;
+            curColor = Color.Black;
+            Pen pen = new Pen(curColor, thickness);
+            Graphics g = Graphics.FromImage(pb.Image);
         }
 
         private void fMain_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            SaveInFile();
         }
 
 
         private void pbHolst_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if ((fig != null) && (press) && (!(drPol || drPolEnd || drPolProc)) && (drRect || drEll || drLine || drCir || drSqu))
+            {
+                pb.Image = myIm.bmp;
+                Bitmap bmp2 = new Bitmap(550, 470);
+                Graphics gr = Graphics.FromImage(bmp2);
+                gr.DrawImage(myIm.bmp, 0, 0);
+                pos2 = e.Location;
+                fig.pos2 = pos2;
+                fig.Draw(gr);
+                myIm.bmp_2 = bmp2;
+                pb.Image = myIm.bmp_2;
+                pb.Refresh();
+            }
         }
 
         private void bColor_Click(object sender, EventArgs e)
         {
-
+            if (cdDraw.ShowDialog() == DialogResult.Cancel)
+                return;
+            curColor = cdDraw.Color;
         }
 
         private void bClear_Click(object sender, EventArgs e)
         {
-
+            Graphics g = Graphics.FromImage(pb.Image);
+            g.Clear(Color.White);
+            pb.Refresh();
+            g.Save();
         }
        
     }
@@ -121,4 +281,9 @@ namespace lab_rab_1
     {
 
     }
+    [NonSerialized]
+    public List<Shape> figures;
+    [NonSerialized]
+    public Bitmap bmp, bmp_2;
+    public byte[] image_byte;
 }
